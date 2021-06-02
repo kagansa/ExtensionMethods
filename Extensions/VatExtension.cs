52fde06f0633d62a16050e35df8843824eb33a33
Kağan <kagansaygin@gmail.com>
2021-06-02 12:16:58 +0300

    VatExtensionExample

---
+++ Extensions/VatExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace VatExtensionExample.Extensions
+{
+    public static class VatExtension
+    {
+        public static double Kdv(this double price, int vatRate)
+        {
+            return price + (price * vatRate / 100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using VatExtensionExample.Extensions;
+
+namespace VatExtensionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double productPrice = 100;
+            Console.WriteLine(productPrice.Kdv(18));
+        }
+    }
+}
